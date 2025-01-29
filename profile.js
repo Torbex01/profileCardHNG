0f0df8b1831f6f873529496f51102fdc335de12f
Torbex01 <oluwaobiawonuga@gmail.com>
2025-01-29 21:00:03 +0100

    new corrections

--- profile.js
+++ profile.js
@@ -1,14 +1,11 @@
-// Function to Update UTC Time
+// update UTC Time
 function updateUTCTime() {
     const utcTimeElement = document.getElementById("utc-time");
     const now = new Date();
-    const utcTimeString = now.toUTCString().split(" ")[4]; // Extract HH:MM:SS
+    const utcTimeString = now.toUTCString().split(" ")[4]; 
     utcTimeElement.textContent = utcTimeString;
 }
-
-// Update time every second
 updateUTCTime();
-setInterval(updateUTCTime, 1000);
 
 // Dark Mode Toggle
 const toggleButton = document.getElementById("dark-mode-toggle");
@@ -16,12 +13,12 @@ const toggleButton = document.getElementById("dark-mode-toggle");
 toggleButton.addEventListener("click",() => {
     document.body.classList.toggle("dark-mode");
 
-    // Save user preference in localStorage
+   
     const isDarkMode = document.body.classList.contains("dark-mode");
     localStorage.setItem("dark-mode", isDarkMode);
 });
 
-// Load Dark Mode preference from localStorage
+
 window.addEventListener("load",  () => {
     const isDarkMode = localStorage.getItem("dark-mode") === "true";
     if (isDarkMode) {
